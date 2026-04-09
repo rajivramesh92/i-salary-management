43d84767f2fe9426d4950c4f248e720b3b3425c2
Rajiv Ramesh <rajivramesh92@gmail.com>
2026-04-09 14:14:18 +0530

    fix cors issue

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "*"
+   origins 'https://i-salary-management-front.vercel.app'
 
     resource "*",
       headers: :any,
